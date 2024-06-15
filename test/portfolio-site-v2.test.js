e5ed9e40a7fcff52524a76d353c4abe7c6e94683
Jacob Darker <jacob@darker.software>
2024-06-15 23:57:50 +0100

    Initial commit

---
+++ test/portfolio-site-v2.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const PortfolioSiteV2 = require('../lib/portfolio-site-v2-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/portfolio-site-v2-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new PortfolioSiteV2.PortfolioSiteV2Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
